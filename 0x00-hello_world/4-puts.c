c6b966e931dfb4fe0738c101b52f613f890cabcc
SinoxoloX <athandiwexakile@gmail.com>
2023-02-16 13:19:43 -0800

    learning puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - The main must "Programming is like building a multilingual puzzle and it must follow with a newline.
+ * main - "Programming is like building a multilingual puzzle.
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
